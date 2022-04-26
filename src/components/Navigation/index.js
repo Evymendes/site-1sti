219516542bb52bd8115fa5bad9e5e349e9dd2492
liviavieira <vieira.liviaaa@gmail.com>
2022-04-26 00:51:01 -0300

    make mobile navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,9 +4,14 @@ import { Link } from "gatsby";
 import * as S from "./styled";
 import Logo from "../../images/1sti-logo.svg";
 import LogoHover from "../../images/1sti-logo_hover.svg";
+import Medium from "../../images/medium_icon.svg";
+import Facebook from "../../images/facebook_icon.svg";
+import LinkedIn from "../../images/linkedin_icon.svg";
+import Instagram from "../../images/instagram_icon.svg";
 
 export default function Navigation() {
   const [isEN, setIsEN] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleLang = () => {
     setIsEN(isEN ? false : true);
@@ -22,7 +27,12 @@ export default function Navigation() {
           </S.NavLogoBox>
         </Link>
 
-        <S.NavLinkBox>
+        <S.MenuBtn
+          onClick={() => setIsOpen(isOpen ? false : true)}
+          isOpen={isOpen}
+        />
+        
+        <S.NavLinkBox isOpen={isOpen}>
           <S.NavLink to="/" activeClassName="active">Home</S.NavLink>
           <S.NavLink to="/services" activeClassName="active">Services</S.NavLink>
           <S.NavLink to="/insights" activeClassName="active">Insights</S.NavLink>
@@ -34,6 +44,13 @@ export default function Navigation() {
           >
             {isEN ? 'PT' : 'EN'}
           </S.LangBtn>
+
+          <S.SocialBox>
+            <img src={Medium} alt="Ícone do Medium" />
+            <img src={Facebook} alt="Ícone do Facebook" />
+            <img src={LinkedIn} alt="Ícone do LinkedIn" />
+            <img src={Instagram} alt="Ícone do Instagram" />
+          </S.SocialBox>
         </S.NavLinkBox>
       </S.NavWrapper>
     </S.NavBar>
