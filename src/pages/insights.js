87b5f21a26d2c32e3592d93c544ac121186c66d3
liviavieira <vieira.liviaaa@gmail.com>
2022-05-06 22:19:38 -0300

    componentize seo

--- src/pages/insights.js
+++ src/pages/insights.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import Seo from "../components/seo";
 import Insights from "../components/Insights";
 
 const Blog = () => (
-  <Layout>
-    <Seo title="Insights" />
+  <Layout seo="Insights">
     <Insights />
   </Layout>
 );
