65fb3caf8b5d96fca31a4ade646831886c9eadf9
blabla <blablablabla@gmail.com>
2022-11-04 13:12:28 -0300

    updated 	new file:   src/assets/images/arrow-up-right-blue.svg 	modified:   src/components/ContactContent/index.js 	modified:   src/components/ContactContent/styled.js 	modified:   src/components/Header/index.js

--- src/components/ContactContent/styled.js
+++ src/components/ContactContent/styled.js
@@ -4,35 +4,58 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  background-color: #0B1617;
-  
-  a {
-    color: #38EEFF;
-  }
-`;
+  background-color: #0b1617;
+`
 
 export const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 1440px;
+  /* margin: 0 auto; */
+
   width: 100%;
   padding: 4rem 0;
 
   @media (max-width: 1440px) {
-    padding: 4rem 4rem;
+    /* padding: 4rem 4rem; */
   }
 
   @media (max-width: 768px) {
     padding: 4rem 2rem;
   }
-`;
+`
+
+export const BoxTitleBook = styled.div`
+  padding: 0 5rem;
+
+  @media (max-width: 1440px) {
+    padding: 0 4rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 1rem;
+  }
+`
 
 export const Content = styled.div`
   margin: 0 auto;
-`;
+`
+
+export const BoxDescriptionContact = styled.div`
+  width: 77%;
+  margin: 0 auto;
+  padding-top: 5rem;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding-top: 2rem;
+    padding-left: 1rem;
+  }
+`
 
 export const UpText = styled.div`
   max-width: 700px;
   width: 100%;
+
   font-size: 1.75rem;
   font-weight: 300;
   line-height: 1.2;
@@ -41,14 +64,26 @@ export const UpText = styled.div`
     padding-bottom: 1rem;
   }
 
+  a {
+    padding-left: 10px;
+  }
+
   @media (max-width: 768px) {
     font-size: 1.3em;
     line-height: 1.5;
   }
 `
+
+export const BoxBook = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
 export const LineBox = styled.section`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
+
   padding: 5rem 0 2rem;
   width: 100%;
   font-size: 1rem;
@@ -62,7 +97,7 @@ export const LineBox = styled.section`
       width: 100%;
     }
   }
-`;
+`
 
 export const Boxes = styled.div`
   display: flex;
@@ -82,7 +117,11 @@ export const Boxes = styled.div`
     letter-spacing: 0.125rem;
     font-weight: 500;
   }
-`;
+
+  a {
+    color: #fff;
+  }
+`
 
 export const BookLink = styled.div`
   margin: 1rem 0 2rem;
@@ -92,16 +131,17 @@ export const BookLink = styled.div`
   img {
     width: 1.5rem;
   }
-  
+
   a {
+    color: #38eeff;
     margin: 0 10px;
     font-weight: 300;
   }
 
   span {
-    color: #38EEFF;
+    color: #38eeff;
     font-size: 1.3em;
-    transition: .3s;
+    transition: 0.3s;
   }
 
   img,
@@ -124,7 +164,7 @@ export const BookLink = styled.div`
       }
     }
   }
-`;
+`
 
 export const NewsLetter = styled.input`
   background-color: transparent;
@@ -137,21 +177,21 @@ export const NewsLetter = styled.input`
   font-size: 1.25rem;
 
   ::placeholder {
-    color: #FFF;
+    color: #fff;
     font-size: 1rem;
     opacity: 1;
   }
-`;
+`
 
 export const Image = styled.img`
-  box-shadow: 0 0 25px #38EEFF;
-`;
+  box-shadow: 0 0 25px #38eeff;
+`
 
 export const Icon = styled.img`
   width: 1.2rem;
   padding-top: 1rem;
-  fill: #FF0070;
-`;
+  fill: #ff0070;
+`
 
 export const Label = styled.div`
   display: flex;
@@ -163,4 +203,4 @@ export const Label = styled.div`
   &:hover button {
     transform: translateX(5px);
   }
-`;
+`
